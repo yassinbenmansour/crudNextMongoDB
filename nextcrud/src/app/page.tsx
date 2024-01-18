6d264aa2c7329c27d070566920c0dff7ddb0d324
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2024-01-18 03:31:02 +0100

    correct syntaxe

--- nextcrud/src/app/page.tsx
+++ nextcrud/src/app/page.tsx
@@ -3,9 +3,6 @@ const mongoose = require('mongoose')
 import dbConnect from "./dbConnect";
 import { redirect } from 'next/navigation'
 
-
-
-
 export default function Home() {
 
 
@@ -19,41 +16,35 @@ export default function Home() {
       dbConnect();
       let newNote = new Todo({title , note});
       await newNote.save();
-      
 
     }catch(error){
       console.log(error)
     }
     redirect(`/show`) // Navigate to the new notes 
-
-
   }
   return (
     <main className='m-10 space-y-5'>
-        <h1 className="text-xl font-bold"> Note</h1>
-
-      <form action={newNote} >
-        <div>
-          <label className="block  text-lg font-medium text-dark ">Title</label>
-          <br />
-          <input
-            type="text"
-            name="title"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-        </div>
-        <div>
-          <label className='block pt-2  text-lh font-medium text-dark'>Note</label>
-          <br />
-          <textarea
-            name="note"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          ></textarea>
-        </div>
-       
-        <button  type="submit" className="mt-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Submit</button>
-
-      </form>
+      <h1 className="text-xl font-bold"> Note</h1>
+        <form action={newNote} >
+          <div>
+            <label className="block  text-lg font-medium text-dark ">Title</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div>
+            <label className='block pt-2  text-lh font-medium text-dark'>Note</label>
+            <br />
+            <textarea
+              name="note"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            ></textarea>
+          </div>
+          <button  type="submit" className="mt-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Submit</button>
+        </form>
 
     </main>
   )
--- nextcrud/src/app/show/page.tsx
+++ nextcrud/src/app/show/page.tsx
@@ -5,14 +5,11 @@ import dbConnect from "../dbConnect";
 
 export default async function show() {
   dbConnect()
-
   const todos = await Todo.find();
-
 
   //function to delete note
   async function deleteNote(data : any) {
     "use server";
-
     let id = JSON.parse(data.get("id")?.valueOf());
 
     await Todo.deleteOne({ _id: id });
@@ -22,63 +19,56 @@ export default async function show() {
   return (
     <main className="m-10 space-y-5">
       <h1 className="text-xl font-bold">Notes</h1>
-
-
-
-    <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right ">
-            <thead className="text-xs  text-white uppercase bg-gray-300  dark:text-white">
-                <tr>
-                    <th scope="col" className="px-6 py-3">
-                        Title
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Description
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Actions
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-            {todos.map((element : any) => {
-            return (
-                <>
-                <tr key={element._id} className="bg-white dark:bg-gray-800 ">
-                    <th scope="row" className="px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {element.title}
-                    </th>
-                    <td className="px-6">
-                        {element.note}
-                    </td>
-                    <td className="flex">
-                        <form action={deleteNote}>
-                            <input type="hidden" value={JSON.stringify(element._id)} name="id"/>
-                        <button
-                            type="submit"
-                            className="p-2 m-2 bg-red-600 rounded text-white hover:cursor-pointer"
-                        >
-                            Delete
-                        </button>
-                        </form>
-
-                        {/* <Delete id={element._id}/> */}
-                        <Link href={"/Edit/" + element._id}>
-                        <button className="p-2 m-2 bg-green-500 rounded text-white hover:cursor-pointer">
-                            Edit
-                        </button>
-                        </Link>
-                    </td>
-                    
-                </tr>
-                </>
-            );
-            })}
-            </tbody>
-        </table>
-    </div>
-
-     
+        <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right ">
+                <thead className="text-xs  text-white uppercase bg-gray-300  dark:text-white">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Title
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Description
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map((element : any) => {
+                    return (
+                        <>
+                        <tr key={element._id} className="bg-white dark:bg-gray-800 ">
+                            <th scope="row" className="px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {element.title}
+                            </th>
+                            <td className="px-6">
+                                {element.note}
+                            </td>
+                            <td className="flex">
+                                <form action={deleteNote}>
+                                    <input type="hidden" value={JSON.stringify(element._id)} name="id"/>
+                                <button
+                                    type="submit"
+                                    className="p-2 m-2 bg-red-600 rounded text-white hover:cursor-pointer"
+                                >
+                                    Delete
+                                </button>
+                                </form>
+                                {/* <Delete id={element._id}/> */}
+                                <Link href={"/Edit/" + element._id}>
+                                <button className="p-2 m-2 bg-green-500 rounded text-white hover:cursor-pointer">
+                                    Edit
+                                </button>
+                                </Link>
+                            </td>
+                        </tr>
+                        </>
+                    );
+                    })}
+                </tbody>
+            </table>
+        </div>
     </main>
   );
 }
